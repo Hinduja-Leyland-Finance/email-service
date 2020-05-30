947c680ff6e5656fb9140a3d3167bd61de0a8392
KshitijBahul <chunks.3@gmail.com>
2020-05-30 17:18:06 +0530

    Added some loggers

--- src/main/java/com/pixels/te/emailService/service/EmailService.java
+++ src/main/java/com/pixels/te/emailService/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.pixels.te.emailService.service;
 
 import com.pixels.te.emailService.domain.MessageCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 @Service
+@Slf4j
 public class EmailService {
     @Autowired
     public JavaMailSender emailSender;
 
     @Async
     public void sendEmail(MessageCommand emailMessage) {
+        log.info("Sending an mail as {}",emailMessage.toString());
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(emailMessage.getToEmail());
         message.setSubject(emailMessage.getSubject());
